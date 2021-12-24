baacb0cb08207902730c03a4c07307c99ab01388
Alex-Gutierrez-SM <96356608+Alex-Gutierrez-SM@users.noreply.github.com>
2021-12-24 16:20:12 +0100

    Add files via upload

--- proyectoPesoPokemon/scripts/scripts.js
+++ proyectoPesoPokemon/scripts/scripts.js
@@ -15,7 +15,8 @@ async function cargarPokemon(){
         //Pasa los datos y da forma a la carta pokémon.
         document.getElementById("idNombrePokemon").innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1);
         document.getElementById("idPesoPokemon").innerHTML = data.weight/10+"  kg";
-        document.getElementById("fotoCarta").src = data["sprites"]["other"]["official-artwork"]["front_default"];
+        // document.getElementById("fotoCarta").src = data["sprites"]["other"]["official-artwork"]["front_default"];
+        document.getElementById("fotoCarta").src = data.sprites.other['official-artwork'].front_default;
         document.getElementById("fotoCarta").classList.add("fotoCarta");
         document.getElementById("contenedorCartaPokemon").style.display = "block";
 
@@ -23,17 +24,18 @@ async function cargarPokemon(){
         let type = data.types[0].type.name;
         const resTipos = await fetch("ajax/tiposPokemon.json");
         const dataTipos = await resTipos.json();
-        let i=0;
-        while (i < dataTipos.length) {
+
+        var i = 0;
+        let dataTiposLen = dataTipos.length;
+        for(i; i < dataTiposLen; i++){
             if(type == dataTipos[i].tipo){
                 document.getElementById("pesoCarta").style.background = dataTipos[i].color;
             }
-            i++;
         }
     }catch(error){
         //Esconde la carta pokemon antes de mostrar el mensaje de error, además de volver a mostrar la foto de Ash.
         document.getElementById("contenedorCartaPokemon").style.display = "none";
-        document.getElementById("fotoAsh").style.display = "hide";
+        document.getElementById("fotoAsh").style.display = "none";
 
         //Muestra el contenedor de error.
         document.getElementById("contenedorErrorWrapper").style.display = "block";
